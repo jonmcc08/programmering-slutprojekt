009d1be30adffa238ecf4e757e7c8adeb9b8017c
Jonathan <jonathan.mccullagh@skola.taby.se>
2026-04-20 11:20:56 +0200

    Testar movement med i form av kolsolen för att hantera x-värde

--- script.js
+++ script.js
@@ -4,12 +4,53 @@ const canvas = document.getElementById("gameCanvas")
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d")
 
-function gameLogic() {
-    
+// Testar movement
+class Book {
+    constructor() {
+        this.path = [{x:630, y:0}, {x: 630, y:260}, {x:100, y:260}, {x:100, y:100}, {x:260, y:100}, {x:260, y:450}, {x:770, y:450}, {x:770, y:250}, {x:918, y:250}]
+        this.x = this.path[0].x
+        this.y = this.path[0].y
+        this.speed = 5
+        this.pathindex = 0
+    }
+
+    movement() {
+
+        const nextPosition = this.path[this.pathindex]
+
+        const disX = nextPosition.x - this.x
+        const disY = nextPosition.y - this.y
+        
+        console.log(disX)
+        console.log(disY)
+        console.log(this.path[this.pathindex])
+
+        if (Math.abs(disX) < this.speed && Math.abs(disY) < this.speed) {
+            this.pathindex++
+        } else {
+            if (disX > 0) {
+                this.x += this.speed
+            } else if (disX < 0) {
+                this.x -= this.speed
+            }
+            if (disY > 0) {
+                this.y += this.speed;
+            } else if (disY < 0) {
+                this.y -= this.speed
+            }
+    }
+    }
+}
+
+let book = new Book()
+// Test
+for(let i = 0; i < 1000; i++) {
+    book.movement()
+    console.log(`{${book.x}: ${book.y}} Index: ${book.pathindex}`)
 }
 
 function animate() {
-
+    
 }
 
 // Pathtracar som template för senare backgrundsbild som ska göras.
@@ -32,3 +73,5 @@ function mapPath() {
 }
 
 mapPath()
+
+
